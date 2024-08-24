bd36a266447a6dccdc281cf6470588eb9b0e9610
Vanashree Pinjari <vanashreepinjari@gmail.com>
2024-08-24 14:28:38 +0530

    Flask_app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<center><h1>Hello, Azure Web Apps</h1></center>!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
